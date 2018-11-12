f61a4e84e8bbf69805067ec38406827d76711c1e
Shivam <shivam160291@gmail.com>
2018-11-12 22:16:35 +0530

    add button routes

---
+++ routes/buttons.route.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const buttons_controller = require('../controllers/buttons.controller');
+
+// a simple test url to check that all of our files are communicating correctly.
+// router.post('/create', redeemer_controller.redeemer_create);
+// router.get('/:id', redeemer_controller.redeemer_details);
+// router.put('/:id/update', redeemer_controller.redeemer_update);
+// router.delete('/:id/delete', redeemer_controller.redeemer_delete);
+
+router.get('/', function(req, res) {
+    res.send('AYO!');
+});
+
+
+
+module.exports = router;
