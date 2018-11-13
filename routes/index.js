94d700c8fa961b9272820036ee2cc2b087411250
Shivam <shivam160291@gmail.com>
2018-11-13 12:13:20 +0530

    working on auth flow

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 // Get Homepage
-router.get('/', function(req, res){
+router.get('/', ensureAuthenticated, function(req, res){
 	res.render('dashboard');
 });
 
 router.get('/login', function(req, res){
-	res.render('auth/login');
+	// res.render('auth/login');
+	//while rendering the view we can specify the laout to be used
+	res.render('auth/login', { title: 'my other page', layout: 'auth' });
 });
 
-// function ensureAuthenticated(req, res, next){
-// 	if(req.isAuthenticated()){
-// 		return next();
-// 	} else {
-// 		//req.flash('error_msg','You are not logged in');
-// 		res.redirect('/users/login');
-// 	}
-// }
+router.post('/login', function(req, res){
+	// res.render('auth/login');
+	console.log(req.body);
+	//while rendering the view we can specify the laout to be used
+	res.redirect('/login');
+});
+
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		//req.flash('error_msg','You are not logged in');
+		res.redirect('/login');
+	}
+}
 
 module.exports = router;
